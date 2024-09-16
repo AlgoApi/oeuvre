0c6acc7907a69f28e9827da250182b2b0c39ed73
KaraGOD <gasdiscord@gmail.com>
2024-09-16 18:04:10 +0500

    ver 1.1

--- oeuvre_master/views.py
+++ oeuvre_master/views.py
@@ -22,7 +22,7 @@ def update(request):
         repo = git.Repo("oeuvre")
         origin = repo.remotes.origin
 
-        origin.pull(autostash=True)
+        origin.pull(autostash=True, rebase=True)
 
         return HttpResponse("Updated code on PythonAnywhere")
     else:
