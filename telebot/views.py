bcdbcde351ad839e55bb00922098ef0204e039f7
KaraGOD <gasdiscord@gmail.com>
2024-09-22 23:36:25 +0500

    ver 1.7 проверка id агентов

--- telebot/views.py
+++ telebot/views.py
@@ -37,7 +37,7 @@ def tguser(request):
             privkeyC = rsa.PrivateKey.load_pkcs1(f.read())
 
         id_agent = request.GET.getlist('id', default="unknown")
-        return HttpResponse(str(type(id_agent)))
+        return HttpResponse(str(type(id_agent[0])))
         clear_id_agent = rsa.decrypt(id_agent, privkeyC).decode()
         mode = request.GET.getlist('mode', default="unknown")
         id_agents = AgentId.objects.all()
