c0daf962a9aee2ff2caba1b37c34630e314b94b3
KaraGOD <gasdiscord@gmail.com>
2024-09-29 15:17:48 +0500

    ver 1.7 проверка id агентов

--- telebot/views.py
+++ telebot/views.py
@@ -41,6 +41,7 @@ class View_tgusers(View):
     @method_decorator(permission_required(perm='telebot.view_tg_users', raise_exception=True), name='dispatch')
     def get(self, request):
         data = ""
+        HttpResponse(request.user.is_authenticated + ", " + request.user.username)
         # получаем все значения модели
         if request.user.is_authenticated:
             id_agent = request.user.username
